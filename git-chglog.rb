9896744b679f582b6d2553de332a520e00554d62
Edoardo T <endorama@users.noreply.github.com>
2019-02-06 12:44:06 +0100

    add linux support
    Update formula to add linux support

--- git-chglog.rb
+++ git-chglog.rb
@@ -5,13 +5,13 @@ HOMEBREW_GIT_VERSION='0.7.1'
 
 class GitChglog < Formula
   homepage "#{REPOSITORY_URL}"
-  url "#{REPOSITORY_URL}/releases/download/#{HOMEBREW_GIT_VERSION}/git-chglog_darwin_amd64"
-  sha256 "d620bbe44c5cf6d669e4a79afd367b99d69a924eb554304a49d3ea255195da47"
+  url "#{REPOSITORY_URL}/releases/download/#{HOMEBREW_GIT_VERSION}/git-chglog_linux_amd64"
+  sha256 "b3c7070e3a53eff952a15693e103861484911a0640b4f28c97e7bc8e72cc0f46"
   head "#{REPOSITORY_URL}.git"
   version "#{HOMEBREW_GIT_VERSION}"
 
   def install
-    system "mv", "git-chglog_darwin_amd64", "git-chglog"
+    system "mv", "git-chglog_linux_amd64", "git-chglog"
     bin.install "git-chglog"
   end
 end
